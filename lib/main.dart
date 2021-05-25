d5182e1a3bf35d2196869b1d2dcba9e92d96b845
FCRx <343ays@gmail.com>
2021-05-25 10:13:43 -0300

    adicionados todo's

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,8 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  /* TODO: Consertos realizados em outros aparelhos parecidos?
+  *  TODO: Como armezenar essas informações? */
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
